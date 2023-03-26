dd7d3a3c66702cdc86caf0e72dc659cdf9313022
lanjian <765797511@qq.com>
2023-03-26 16:20:59 +0800

    Update main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -17,6 +17,14 @@ unzip -o master.zip
 rm master.zip*
 node scripts/ruan-weekly.js > scripts/ruan-weekly.json
 cp -n ./weekly-master/docs/issue*.md ./docs/weekly/ruan-weekly/
+cd ./docs/weekly/ruan-weekly/
+# add pv to markdown / replace link
+files=`ls . | grep issue`
+for file in ${files[@]}
+do 
+    sed -i '3i\<img class="pv" src="https://api.visitor.plantree.me/visitor-badge/pv?namespace=plantree.me&key=ruanyf-weekly/'${file}'">\n' ${file}
+    sed -i 's/http:\/\/www.ruanyifeng.com\/blog\/.*issue-\([0-9]\+\)\.html/.\/issue-\1/' ${file}
+done
 
 # HelloGitHub开源的月刊
 curl -s -L -O https://github.com/521xueweihan/HelloGitHub/archive/refs/heads/master.zip
