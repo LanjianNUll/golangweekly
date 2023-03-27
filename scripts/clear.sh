6a7379b4c74565a132e913ae79b7a1612415dfe9
lanjian <765797511@qq.com>
2023-03-27 10:29:51 +0800

    更新clear.sh

---
+++ scripts/clear.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+rm -rf docs/weekly/golang-weekly/issue-*
+
+rm -rf docs/weekly/golang-weekly/imgs/
+
+rm -rf docs/weekly/golang-weekly/res/
+
+rm -rf docs/weekly/ruan-weekly/issue-*
+
+rm -rf docs/weekly/hello-github/HelloGitHub*
+
+rm -rf scripts/*.json
